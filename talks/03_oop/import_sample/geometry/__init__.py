e2558d4f71f14ca60c6ceee57b97ba767ffca010
VadimPushtaev <pushtaev.vm@gmail.com>
2018-01-11 21:05:11 +0300

    oop

--- talks/03_oop/import_sample/geometry/__init__.py
+++ talks/03_oop/import_sample/geometry/__init__.py
@@ -1,6 +1,6 @@
 import math
 
-def triangle_size(side_a, side_b, angle):
+def triangle_side(side_a, side_b, angle):
     return math.sqrt(
         side_a ** 2 + side_b ** 2 - 2 * side_a * side_b * math.cos(angle)
     )
