9b620ddeac380ca0aa27850e4610f84ed6cb5b95
v.denisov <>
2018-03-23 15:23:04 +0300

    исправления по фаилам дз

--- homeworks/log_parse/log_parse.py
+++ homeworks/log_parse/log_parse.py
@@ -1,27 +1,4 @@
 # -*- encoding: utf-8 -*-
-import datetime
-
-
-def get_logs(string):
-    if string and string[0] == '[':
-        try:
-            int(string[-2:])
-            return string
-        except ValueError:
-            return None
-
-
-def get_data(log):
-    start = log.find('GET ')
-    end = log.find(' ', start + 4)
-    url = log[start + 4: end + 1]
-    request_time = log[log.rfind(' '):]
-    url = url.replace('https://', '')
-    url = url.replace('http://', '')
-    pos = url.find('?')
-    url = url[0: pos]
-    return url, request_time
-
 
 def parse(
     ignore_files=False,
