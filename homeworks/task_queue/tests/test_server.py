b25843c1c3cb8a0e17dc821c05d15bd0d625b6e0
v.ksenz <v.ksenz@corp.mail.ru>
2018-04-17 23:37:45 +0300

    task queue tests example added

---
+++ homeworks/task_queue/tests/test_server.py
@@ -0,0 +1,55 @@
+from unittest import TestCase
+
+import time
+import socket
+
+import subprocess
+
+from server import run
+
+
+class ServerBaseTest(TestCase):
+    def setUp(self):
+        self.server = subprocess.Popen(['python', 'server.py'])
+        # даем серверу время на запуск
+        time.sleep(0.5)
+
+    def tearDown(self):
+        self.server.terminate()
+        self.server.wait()
+
+    def send(self, command):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('127.0.0.1', 5555))
+        s.send(command)
+        data = s.recv(1000000)
+        s.close()
+        return data
+
+    def test_base_scenario(self):
+        task_id = self.send(b'ADD 1 5 12345')
+        self.assertEqual(b'YES', self.send(b'IN 1 ' + task_id))
+
+        self.assertEqual(task_id + b' 5 12345', self.send(b'GET 1'))
+        self.assertEqual(b'YES', self.send(b'IN 1 ' + task_id))
+        self.assertEqual(b'YES', self.send(b'ACK 1 ' + task_id))
+        self.assertEqual(b'NO', self.send(b'ACK 1 ' + task_id))
+        self.assertEqual(b'NO', self.send(b'IN 1 ' + task_id))
+
+    def test_two_tasks(self):
+        first_task_id = self.send(b'ADD 1 5 12345')
+        second_task_id = self.send(b'ADD 1 5 12345')
+        self.assertEqual(b'YES', self.send(b'IN 1 ' + first_task_id))
+        self.assertEqual(b'YES', self.send(b'IN 1 ' + second_task_id))
+
+        self.assertEqual(first_task_id + b' 5 12345', self.send(b'GET 1'))
+        self.assertEqual(b'YES', self.send(b'IN 1 ' + first_task_id))
+        self.assertEqual(b'YES', self.send(b'IN 1 ' + second_task_id))
+        self.assertEqual(second_task_id + b' 5 12345', self.send(b'GET 1'))
+
+        self.assertEqual(b'YES', self.send(b'ACK 1 ' + second_task_id))
+        self.assertEqual(b'NO', self.send(b'ACK 1 ' + second_task_id))
+
+
+if __name__ == '__main__':
+    unittest.main()
