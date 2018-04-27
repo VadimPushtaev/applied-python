fbf819a9c3d96a351733d50a96cc251678d4ba3e
VadimPushtaev <pushtaev.vm@gmail.com>
2018-04-27 17:23:55 +0300

    hackathon

---
+++ hackathons/bot/bot.py
@@ -0,0 +1,56 @@
+import os
+import time
+import re
+from slackclient import SlackClient
+
+from command_pool import CommandPool
+import command_handler.sample
+
+# instantiate Slack client
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+# starterbot's user ID in Slack: value is assigned after the bot starts up
+starterbot_id = None
+
+# constants
+RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+EXAMPLE_COMMAND = "do"
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+def parse_bot_commands(slack_events):
+    for event in slack_events:
+        if event["type"] == "message" and not "subtype" in event:
+            user_id, message = parse_direct_mention(event["text"])
+            if user_id == starterbot_id:
+                return message, event["channel"]
+    return None, None
+
+def parse_direct_mention(message_text):
+    matches = re.search(MENTION_REGEX, message_text)
+    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+def handle_command(command_pool, command_text):
+    result = command_pool.handle(command_text)
+
+    if result:
+        slack_client.api_call(
+            "chat.postMessage",
+            channel=channel,
+            text=result,
+         )
+
+if __name__ == "__main__":
+    command_pool = CommandPool()
+
+    if slack_client.rtm_connect(with_team_state=False):
+        print("Starter Bot connected and running!")
+        # Read bot's user ID by calling Web API method `auth.test`
+        starterbot_id = slack_client.api_call("auth.test")["user_id"]
+        while True:
+            command_text, channel = parse_bot_commands(slack_client.rtm_read())
+            if command_text:
+                handle_command(command_pool, command_text)
+            time.sleep(RTM_READ_DELAY)
+    else:
+        print("Connection failed. Exception traceback printed above.")
+    pass
+
---
+++ hackathons/bot/command_handler/sample.py
@@ -0,0 +1,12 @@
+from command_pool import CommandPool
+from command_handler import CommandHandler
+
+@CommandPool.register_command_class
+class SampleCommandHandler(CommandHandler):
+    def handle(self, text):
+        if text.startswith('_'):
+            return text
+
+        if text.startswith('@'):
+            raise RuntimeError(text)
+
