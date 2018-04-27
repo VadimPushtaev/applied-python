bcc00a8983e6670f3019b2f6bc7304c5814365ac
KostyaBobroff <k.bobrov.jr@gmail.com>
2018-04-27 21:08:07 +0300

    change commands in Message

--- hackathons/bot/tests/command_handler/Test_message.py
+++ hackathons/bot/tests/command_handler/Test_message.py
@@ -8,16 +8,17 @@ class MyTestCase(unittest.TestCase):
         obj = MessageCommandHandler()
 
         for i in range(5):
-            obj.handle('_start yeah' + str(i))
+            obj.handle('messages start yeah' + str(i))
         return obj
 
     def test_get(self):
         obj = self.Obj()
-        self.assertEqual('yeah4', obj.handle('_get'))
+        self.assertEqual('yeah4', obj.handle('messages get'))
 
     def test_random(self):
         obj = self.Obj()
-        self.assertEqual('yeah3', obj.handle('_random', rand_func=lambda x, b: 3))
+        print('asd')
+        self.assertEqual('yeah3', obj.handle('messages random', rand_func=lambda x, b: 3))
 
 
 if __name__ == '__main__':
