e5aaf5315192c7d25a276bcd2b8919bb503024f3
VadimPushtaev <pushtaev.vm@gmail.com>
2018-05-07 20:52:47 +0300

    async

---
+++ talks/07_async/server.py
@@ -0,0 +1,13 @@
+from aiohttp import web
+import asyncio
+
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/{name}', handle)])
+
+web.run_app(app)
