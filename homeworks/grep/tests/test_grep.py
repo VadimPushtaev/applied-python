4221d11e00a7e6fcd4cdb05306ebd6ea7eb595e0
V.Ksenz <leesasp@gmail.com>
2018-09-28 00:28:40 +0300

    added tests for grep

--- homeworks/grep/tests/test_grep.py
+++ homeworks/grep/tests/test_grep.py
@@ -16,7 +16,7 @@ grep.output = save_to_list
 
 class GrepBaseTest(TestCase):
 
-    lines = ['baab', 'bbb', 'ccc']
+    lines = ['baab', 'bbb', 'ccc', 'A']
 
     def tearDown(self):
         global lst
@@ -40,4 +40,87 @@ class GrepBaseTest(TestCase):
     def test_base_scenario_invert(self):
         params = grep.parse_args(['-v', 'b'])
         grep.grep(self.lines, params)
-        self.assertEqual(lst, ['ccc'])
+        self.assertEqual(lst, ['ccc', 'A'])
+
+    def test_base_scenario_case(self):
+        params = grep.parse_args(['-i', 'a'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['baab', 'A'])
+
+
+class GrepPatternTest(TestCase):
+
+    lines = ['baab', 'abbb', 'fc', 'AA']
+
+    def tearDown(self):
+        global lst
+        lst.clear()
+
+    def test_question_base(self):
+        params = grep.parse_args(['?b'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['baab', 'abbb'])
+
+    def test_question_start(self):
+        params = grep.parse_args(['?a'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['baab'])
+
+    def test_queston_end(self):
+        params = grep.parse_args(['c?'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, [])
+
+    def test_queston_double(self):
+        params = grep.parse_args(['b??b'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['baab'])
+
+    def test_queston_count(self):
+        params = grep.parse_args(['???'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['baab', 'abbb'])
+
+    def test_asterics(self):
+        params = grep.parse_args(['b*b'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['baab', 'abbb'])
+
+    def test_asterics_all(self):
+        params = grep.parse_args(['***'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, self.lines)
+
+class GrepContextTest(TestCase):
+
+    lines = ['vr','baab', 'abbb', 'fc', 'bbb', 'cc']
+
+    def tearDown(self):
+        global lst
+        lst.clear()
+
+    def test_context_base(self):
+        params = grep.parse_args(['-C1','aa'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['vr', 'baab', 'abbb'])
+
+    def test_context_intersection(self):
+        params = grep.parse_args(['-C1','ab'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['vr', 'baab', 'abbb', 'fc'])
+
+    def test_context_intersection_hard(self):
+        params = grep.parse_args(['-C2','bbb'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, self.lines)
+
+    def test_before(self):
+        params = grep.parse_args(['-B1','bbb'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['baab', 'abbb', 'fc', 'bbb'])
+
+    def test_after(self):
+        params = grep.parse_args(['-A1','bbb'])
+        grep.grep(self.lines, params)
+        self.assertEqual(lst, ['abbb', 'fc', 'bbb', 'cc'])
+
